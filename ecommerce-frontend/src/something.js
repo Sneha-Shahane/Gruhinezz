de011fc2c5e2257e927972d0d94fa9b92bde5e14
KD <123051736+mannerinfo@users.noreply.github.com>
2025-09-20 21:48:24 +0530

    Create something.js
    lav onclick

---
+++ ecommerce-frontend/src/something.js
@@ -0,0 +1,96 @@
+import React from "react";
+import rectangle806 from "./rectangle-806.png";
+import rectangle843 from "./rectangle-843.png";
+import "./style.css";
+
+export const Frame = () => {
+    return (
+        <div className="frame">
+            <div className="rectangle" />
+
+            <div className="div" />
+
+            <div className="rectangle-2" />
+
+            <div className="rectangle-3" />
+
+            <div className="div-2" />
+
+            <div className="rectangle-4" />
+
+            <div className="rectangle-5" />
+
+            <div className="rectangle-6" />
+
+            <img className="img" alt="Rectangle" src={rectangle806} />
+
+            <img className="rectangle-7" alt="Rectangle" src={rectangle843} />
+
+            <div className="rectangle-8" />
+
+            <div className="rectangle-9" />
+
+            <div className="rectangle-10" />
+
+            <div className="text-wrapper">Nagli Papad</div>
+
+            <div className="text-wrapper-2">150 $</div>
+
+            <div className="text-wrapper-3">150 $</div>
+
+            <div className="text-wrapper-4">150 $</div>
+
+            <div className="text-wrapper-5">150 $</div>
+
+            <div className="text-wrapper-6">150 $</div>
+
+            <div className="text-wrapper-7">150 $</div>
+
+            <div className="text-wrapper-8">Product 4</div>
+
+            <div className="text-wrapper-9">Mayuri Garude</div>
+
+            <div className="text-wrapper-10">200 $</div>
+
+            <div className="text-wrapper-11">200 $</div>
+
+            <div className="text-wrapper-12">200 $</div>
+
+            <div className="text-wrapper-13">200 $</div>
+
+            <div className="text-wrapper-14">200 $</div>
+
+            <div className="text-wrapper-15">200 $</div>
+
+            <div className="text-wrapper-16">|</div>
+
+            <div className="text-wrapper-17">|</div>
+
+            <div className="text-wrapper-18">|</div>
+
+            <div className="text-wrapper-19">|</div>
+
+            <div className="text-wrapper-20">|</div>
+
+            <div className="text-wrapper-21">|</div>
+
+            <div className="text-wrapper-22">From Someone</div>
+
+            <div className="text-wrapper-23">From Someone</div>
+
+            <div className="text-wrapper-24">From Someone</div>
+
+            <div className="text-wrapper-25">From Someone</div>
+
+            <div className="text-wrapper-26">From Someone</div>
+
+            <div className="text-wrapper-27">Product 2</div>
+
+            <div className="text-wrapper-28">Product 5</div>
+
+            <div className="text-wrapper-29">Product 3</div>
+
+            <div className="text-wrapper-30">Product 6</div>
+        </div>
+    );
+};
